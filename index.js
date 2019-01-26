4c90892c1a4f6a770c40becce99d2fcba1290cd1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-01-26 12:06:39 -0500

    this might work

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-fly.http.respondWith(request => new Response("Hello, world!", { status: 200 }));
+import proxy from "@fly/fetch/proxy";
+
+const bundle = {
+  name: "bundle",
+  test: request => request.url.includes("index.js"),
+  respond: request => new Response("static", { status: 200 })
+};
+
+const admin = {
+  name: "admin",
+  test: request => request.referrer && request.referrer.includes("/admin"),
+  respond: request =>
+    proxy(request, `https://${app.config.adminBackend}`, {
+      headers: {
+        host: app.config.adminBackend,
+        "x-forwarded-host": request.headers.get("hostname")
+      }
+    })
+};
+
+const texter = {
+  name: "texter",
+  test: request => true,
+  respond: request => {
+    console.log(app);
+    return proxy(`https://${app.config.texterBackend}`, {
+      headers: {
+        host: app.config.texterBackend,
+        "x-forwarded-host": request.headers.get("hostname")
+      }
+    })(request);
+  }
+};
+
+const backends = [bundle, admin, texter];
+
+fly.http.respondWith(request => {
+  console.log(request);
+  for (let backend of backends) {
+    if (backend.test(request)) {
+      console.log(`Using ${backend.name}`);
+      return backend.respond(request);
+    }
+  }
+});
